87a7ead87a17d2b2bd8edd227e88e5d2a0304765
warmcongee <warmcongee@gmail.com>
2022-07-07 13:35:41 +0800

    Add learning rate decay in CIFAR training

--- CIFARtrain.py
+++ CIFARtrain.py
@@ -37,33 +37,35 @@ images, labels = dataiter.next()
 
 # CreateNet
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('using device:', device)
 print(device)
 net = CIFARNet()
 net.to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.005, momentum=0.9)
+StepLR = torch.optim.lr_scheduler.StepLR(optimizer, step_size=12000, gamma=0.60)
 
 for epoch in range(20):  # loop over the dataset multiple times
-
     running_loss = 0.0
+
+    print("第%d个epoch的学习率：%f" % (epoch, optimizer.param_groups[0]['lr']))
     for i, data in enumerate(train_loader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data[0].to(device), data[1].to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
-
         # forward + backward + optimize
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-
+        StepLR.step()
         # print statistics
         running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.4f}')
             running_loss = 0.0
 
 print('Finished Training')
